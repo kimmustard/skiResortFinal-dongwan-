914bc9955b691d4837d534afea64e2c00efeee6e
ffinky99 <ffinky99@gmail.com>
2023-11-21 18:06:29 +0900

    달력 달력

--- spring-ski-resort/src/main/webapp/WEB-INF/views/hotel/reservation.jsp
+++ spring-ski-resort/src/main/webapp/WEB-INF/views/hotel/reservation.jsp
@@ -6,7 +6,31 @@
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
+
+  <!-- Bootstrap 4 CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+
+    <!-- Bootstrap 4 JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+    <!-- Moment.js -->
+    <script src="https://cdn.jsdelivr.net/npm/moment@2.29.1/moment.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/moment@2.29.1/locale/ko.js"></script> <!-- 한국어 locale 파일 추가 -->
+
+    <!-- DateRangePicker CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/daterangepicker@3.1.0/daterangepicker.css" />
+
+    <!-- DateRangePicker JS -->
+    <script src="https://cdn.jsdelivr.net/npm/daterangepicker@3.1.0/daterangepicker.min.js"></script>
+
+	
 <link rel="stylesheet" href="/resources/css/hotel/reservation.css">
+
+
+
 
 <body>
 	<jsp:include page="../common/nav.jsp"></jsp:include>
@@ -23,9 +47,13 @@
 
 		<div class="box">
 			<form action="">
-				<div class="calendar">
-					<input id="input-id" type="text" />달력z
+				<div class="container mt-3">
+					<label for="dateRangePicker">날짜를 선택하세요:</label> <input type="text"
+						class="form-control" id="dateRangePicker">
 				</div>
+
+				<div id="result1" class="container mt-3"></div>
+				
 				<div class="people">인원수</div>
 				<button id="payBtn" type="button">다음</button>
 				<div id="innerbox" style="display: none;">
@@ -45,7 +73,7 @@
 
 
 
-	
+
 	<script defer type="text/javascript"
 		src="/resources/js/hotel/reservation.js"></script>
 
--- spring-ski-resort/src/main/webapp/resources/js/hotel/reservation.js
+++ spring-ski-resort/src/main/webapp/resources/js/hotel/reservation.js
@@ -10,3 +10,81 @@ document.getElementById("closeBtn").addEventListener('click',()=>{
     	 document.getElementById("innerbox").style.display="none";
    
 })
+ 
+ 	    function updateCustomText(dateRange, resultId) {
+        var customText = "선택한 날짜 범위: " + dateRange;
+        $('#' + resultId).text(customText);
+    }
+
+	function toggleApplyButton(selector, isEnabled) {
+    const applyButton = $(selector).closest('.daterangepicker').find('.applyBtn');
+    if (isEnabled) {
+        applyButton.removeAttr('disabled');
+    } else {
+        applyButton.attr('disabled', 'disabled');
+    }
+}
+
+
+    $(function () {
+        // 한국어 locale 사용
+        moment.locale('ko');
+
+        // 첫 번째 DateRangePicker
+        $('#dateRangePicker').daterangepicker({
+        	"maxSpan": {
+        "days": 10
+    },
+            minDate: moment(),
+            startDate: moment().startOf('hour'),
+    		endDate: moment().startOf('hour').add(32, 'hour'),
+            opens: 'center', 
+            autoUpdateInput: false, 
+            locale: {
+                format: 'YYYY-MM-DD',
+                applyLabel: '적용',
+                cancelLabel: '취소',
+                fromLabel: '시작일',
+                toLabel: '종료일',
+                customRangeLabel: '사용자 정의 범위'
+            }
+        }, function (start, end, label) {
+            var dateRange = start.format('YYYY-MM-DD') + ' ~ ' + end.format('YYYY-MM-DD');
+          if(start.format('YYYY-MM-DD')==end.format('YYYY-MM-DD')){
+              toggleApplyButton('#dateRangePicker', false);
+            }
+            updateCustomText(dateRange, 'result1');
+          
+        });
+        
+        // 초기화면에도 적용하기 위해 한 번 호출
+        var initialDateRange = $('#dateRangePicker').val();
+        updateCustomText(initialDateRange, 'result1');
+
+        // 두 번째 DateRangePicker
+        $('#secondDateRangePicker').daterangepicker({
+            minDate: moment(),  
+            startDate: moment().startOf('hour'),
+    		endDate: moment().startOf('hour').add(32, 'hour'),
+            opens: 'left', 
+            autoUpdateInput: false,
+            locale: {
+                format: 'YYYY-MM-DD',
+                applyLabel: '적용',
+                cancelLabel: '취소',
+                fromLabel: '시작일',
+                toLabel: '종료일',
+                customRangeLabel: '사용자 정의 범위'
+            }
+        }, function (start, end, label) {
+            var dateRange = start.format('YYYY-MM-DD') + ' ~ ' + end.format('YYYY-MM-DD');
+            updateCustomText(dateRange, 'result2');
+        });
+
+     	
+
+    
+
+    });
+    
+    
