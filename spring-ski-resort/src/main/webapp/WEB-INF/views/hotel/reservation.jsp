135802be75ffb0eb5f722de85bc44ec2d9e3bd55
ffinky99 <ffinky99@gmail.com>
2023-11-22 16:08:49 +0900

    no message

--- spring-ski-resort/src/main/webapp/WEB-INF/views/hotel/reservation.jsp
+++ spring-ski-resort/src/main/webapp/WEB-INF/views/hotel/reservation.jsp
@@ -25,9 +25,9 @@
 
     <!-- DateRangePicker JS -->
     <script src="https://cdn.jsdelivr.net/npm/daterangepicker@3.1.0/daterangepicker.min.js"></script>
-
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 	
-<link rel="stylesheet" href="/resources/css/hotel/reservation.css">
+	<link rel="stylesheet" href="/resources/css/hotel/reservation.css">
 
 
 
@@ -48,14 +48,65 @@
 		<div class="box">
 
 			<form action="">
-				<div class="container mt-3">
-					<label for="dateRangePicker">날짜를 선택하세요:</label> <input type="text"
-						class="form-control" id="dateRangePicker">
-				</div>
-
-				<div id="result1" class="container mt-3"></div>
 				
-				<div class="people">인원수</div>
+				<div class="date-group">
+					<div class="input-group calendar">
+              	 	<div class="input-group-prepend">
+                		 <span class="input-group-text"><i class="bi bi-calendar"></i></span>
+                	</div>
+					<input type="text" class="form-control" id="dateRangePicker" placeholder="날짜를 선택하세요" readonly="readonly">
+					</div>
+					
+					<div class="input-group mb-3">
+  						<span class="input-group-text" id="basic-addon1"><i class="bi bi-person-fill"></i></span>
+  						<input type="text" id="people" class="form-control people" placeholder="인원수" aria-label="Username" readonly="readonly" aria-describedby="basic-addon1">
+					</div>
+				</div>
+  				<div class="peoplelist"  id="peoplelist" style="display: none;">
+  				
+  					<div style="height: 25px">
+  						성인    
+  						<div class="people-Btn"> 
+	  						<div>
+	  							<button type="button" id="adult-Btn">-</button>
+	  						</div> 
+	  						 
+	  						<span id="audlt-Count">
+	  							1
+	  						</span> 
+	  						
+	  						<div>
+	  							<button type="button" id="adult+Btn">+</button>
+	  						</div> 
+  						</div> 
+  					</div>
+  					
+  					<div style="height: 25px">
+  						어린이
+  						<div class="people-Btn"> 
+	  						<div>
+	  							<button type="button" id="child-Btn">-</button>
+	  						</div> 
+	  						 
+	  						<span id="child-Count">
+	  							0
+	  						</span> 
+	  						
+	  						<div>
+	  							<button type="button" id="child+Btn">+</button>
+	  						</div> 
+  						</div> 
+  						
+  						
+  						<div id="chile-age">
+  						</div>
+  						
+  					</div>
+  				</div>
+  				
+				
+				
+				
 				<button id="payBtn" type="button">다음</button>
 				<div id="innerbox" style="display: none;">
 					<input type="radio" name="room" value="방">방 <input
--- spring-ski-resort/src/main/webapp/resources/js/hotel/reservation.js
+++ spring-ski-resort/src/main/webapp/resources/js/hotel/reservation.js
@@ -1,6 +1,5 @@
 document.getElementById("payBtn").addEventListener('click',()=>{
    
-
         document.getElementById("innerbox").style.display="block";
    
    
@@ -11,10 +10,21 @@ document.getElementById("closeBtn").addEventListener('click',()=>{
    
 })
  
- 	    function updateCustomText(dateRange, resultId) {
-        var customText = "선택한 날짜 범위: " + dateRange;
-        $('#' + resultId).text(customText);
-    }
+ 	function updateCustomText(dateRange, resultId) {
+  	  var startDate = moment(dateRange.split(' ~ ')[0]);
+   	 var endDate = moment(dateRange.split(' ~ ')[1]);
+     if( getDayOfWeek(startDate)=="Invalid date"){
+
+     }else{
+   	 var customText = "예약 날짜 : " + dateRange + " (" + getDayOfWeek(startDate) + " ~ " + getDayOfWeek(endDate) + ")";
+          $('#' + resultId).attr('placeholder',customText);
+     }
+}
+
+function getDayOfWeek(date) {
+    // moment.js를 사용하여 요일 문자열 가져오기
+    return date.format('dddd');
+}
 
 	function toggleApplyButton(selector, isEnabled) {
     const applyButton = $(selector).closest('.daterangepicker').find('.applyBtn');
@@ -24,7 +34,6 @@ document.getElementById("closeBtn").addEventListener('click',()=>{
         applyButton.attr('disabled', 'disabled');
     }
 }
-
 
     $(function () {
         // 한국어 locale 사용
@@ -53,7 +62,7 @@ document.getElementById("closeBtn").addEventListener('click',()=>{
           if(start.format('YYYY-MM-DD')==end.format('YYYY-MM-DD')){
               toggleApplyButton('#dateRangePicker', false);
             }
-            updateCustomText(dateRange, 'result1');
+            updateCustomText(dateRange, 'dateRangePicker');
           
         });
         
@@ -76,15 +85,73 @@ document.getElementById("closeBtn").addEventListener('click',()=>{
                 toLabel: '종료일',
                 customRangeLabel: '사용자 정의 범위'
             }
-        }, function (start, end, label) {
-            var dateRange = start.format('YYYY-MM-DD') + ' ~ ' + end.format('YYYY-MM-DD');
-            updateCustomText(dateRange, 'result2');
-        });
-
-     	
-
-    
+        } );
 
     });
     
+    //성인 버튼 조작
+    document.getElementById("adult+Btn").addEventListener('click',()=>{
+       let cnt =  parseInt(document.getElementById('audlt-Count').innerText); 
+       if(cnt<30){
+        cnt += 1;
+     }
+   
+       document.getElementById('audlt-Count').innerText =cnt;
+       AllPeopleCount();
+     })
+     
+      document.getElementById("adult-Btn").addEventListener('click',()=>{
+       let cnt =  parseInt(document.getElementById('audlt-Count').innerText); 
+      if(cnt>1){
+       cnt -= 1;
+    }
+    document.getElementById('audlt-Count').innerText =cnt;
+    AllPeopleCount();
+     })
     
+    
+    //어린이 버튼 조작
+    document.getElementById("child+Btn").addEventListener('click',()=>{
+       let cnt =  parseInt(document.getElementById('child-Count').innerText); 
+       if(cnt<10){
+        cnt += 1;
+     }
+      
+       document.getElementById('child-Count').innerText =cnt;
+       AllPeopleCount();
+       childAgeSelectCreater();
+     })
+     
+      document.getElementById("child-Btn").addEventListener('click',()=>{
+       let cnt =  parseInt(document.getElementById('child-Count').innerText); 
+      if(cnt>0){
+       cnt -= 1;
+    }
+        document.getElementById('child-Count').innerText =cnt;
+     AllPeopleCount();
+     childAgeSelectCreater();
+     })
+    
+    function AllPeopleCount(){
+        $('#people').attr('placeholder',`성인 : ${document.getElementById('audlt-Count').innerText}명 · 아동 ${document.getElementById('child-Count').innerText}명 ` );
+    }
+    function childAgeSelectCreater(){
+       
+        let select ="";
+        for(let i = 1; i<=parseInt(document.getElementById('child-Count').innerText); i++){
+            
+            select+= `<select class="child-age-selecr">`;
+            select+=`<option value="">나이(필수 입력)</option>`;
+            for(let  i =0 ; i <17; i++){
+                select+= `<option value="${i}">${i}</option>`;
+            
+        }
+        select += `</select>`;   
+         }
+         console.log(select);
+         document.getElementById('chile-age').innerHTML = select;
+     
+    }
+document.getElementById('people').addEventListener('click',()=>{
+    document.getElementById('peoplelist').style.display="block";s
+})
