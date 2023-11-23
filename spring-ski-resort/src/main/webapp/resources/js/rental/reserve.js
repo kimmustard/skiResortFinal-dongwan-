8ed5a1b4e28c779c0130a510a9b08b58bf24dd42
JeonHyeSeong <bc7399@naver.com>
2023-11-23 17:01:32 +0900

    렌탈 js파일 수정

--- spring-ski-resort/src/main/webapp/resources/js/rental/reserve.js
+++ spring-ski-resort/src/main/webapp/resources/js/rental/reserve.js
@@ -1,17 +1,32 @@
-document.addEventListener("DOMContentLoaded", function() {
-        let numberOfPeopleInput = document.getElementById("numberOfPeople");
-        let incrementBtn = document.getElementById("incrementBtn");
-        let decrementBtn = document.getElementById("decrementBtn");
+document.addEventListener('DOMContentLoaded', function () {
+    let dateRangePicker = document.getElementById('dateRangePicker');
+    let rentalReserveStart = document.getElementById('rentalReserveStart');
 
-        incrementBtn.addEventListener("click", function() {
-            let currentNumberOfPeople = parseInt(numberOfPeopleInput.value, 10);
-            numberOfPeopleInput.value = currentNumberOfPeople + 1;
+    if (dateRangePicker && rentalReserveStart) {
+        dateRangePicker.addEventListener('input', function () {
+            rentalReserveStart.value = this.value;
         });
 
-        decrementBtn.addEventListener("click", function() {
-            let currentNumberOfPeople = parseInt(numberOfPeopleInput.value, 10);
-            if (currentNumberOfPeople > 1) {
-                numberOfPeopleInput.value = currentNumberOfPeople - 1;
-            }
+        dateRangePicker.addEventListener('cancel.daterangepicker', function () {
+            rentalReserveStart.value = '';
         });
-   	});
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    let numberOfPeopleInput = document.getElementById("numberOfPeople");
+    let incrementBtn = document.getElementById("incrementBtn");
+    let decrementBtn = document.getElementById("decrementBtn");
+
+    incrementBtn.addEventListener("click", function () {
+        let currentNumberOfPeople = parseInt(numberOfPeopleInput.value, 10);
+        numberOfPeopleInput.value = currentNumberOfPeople + 1;
+    });
+
+    decrementBtn.addEventListener("click", function () {
+        let currentNumberOfPeople = parseInt(numberOfPeopleInput.value, 10);
+        if (currentNumberOfPeople > 1) {
+            numberOfPeopleInput.value = currentNumberOfPeople - 1;
+        }
+    });
+});
